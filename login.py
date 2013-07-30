71934b04e4bf31b6cf28af17b685124c1683eeec
gimli64 <lucasdchamps@gmail.com>
2013-07-30 20:02:41 +0200

    Removing useless images

--- login.py
+++ login.py
@@ -3,14 +3,14 @@ from local_conf import PATH_TO_T4C
 
 start(PATH_TO_T4C)
 
-wait_until(Image(r"images\connect.jpg").exists, timeout_secs=10)
+wait_until(Image(r"images\connecter.jpg").exists, timeout_secs=10)
 press(ENTER)
 
-wait_until(Image(r"images\news.jpg").exists, timeout_secs=100)
+wait_until(Image(r"images\fleche.jpg").exists, timeout_secs=100)
 press(ENTER)
 
-wait_until(Image(r"images\enter_realms.jpg").exists, timeout_secs=10)
+wait_until(Image(r"images\jouer.jpg").exists, timeout_secs=10)
 press(ENTER)
 
-wait_until(Image(r"images\enter.jpg").exists, timeout_secs=10)
+wait_until(Image(r"images\entrer.jpg").exists, timeout_secs=10)
 press(ENTER)
